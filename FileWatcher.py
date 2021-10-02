75805b4821f65a81fe7f9663e81445865fc0e7b5
Adam Ploof <adamploof@hotmail.com>
2021-10-02 14:11:44 -0400

    added methods for adding/removing/ignoring watch files with config manager

--- FileWatcher.py
+++ FileWatcher.py
@@ -1,4 +1,5 @@
 import os, shutil, time, argparse
+import json
 
 from pathlib import Path
 
@@ -41,6 +42,76 @@ class FileChangeWatcher():
             print('An error occured: ')
             print(e)
 
+
+"""
+Structure of config file is:
+    {
+        "watch": {
+            "destination_dir1": [
+                "file1",
+                "file2",
+                ...
+            ],
+            "destination_dir2": [
+                ...
+            ],
+            ...
+        },
+        "ignore": [
+            "file3",
+            "file4",
+            "dir3",
+            ...
+        ]
+    }
+"""
+class ConfigManager():
+    def __init__(self, config_path=None) -> None:
+        if config_path:
+            self.config_path = config_path
+        else:
+            self.config_path = 'wac.config.json'
+
+        if not os.path.exists(self.config_path):
+            create_config_file = input('Could not find config file. Would you like to create one? (y/n):')
+            if create_config_file.lower() == 'y' or create_config_file.lower() == 'yes':
+                with open(self.config_path, 'w+') as f:
+                    blank_config = {
+                        'watch': [],
+                        'ignore': []
+                    }
+                    json.dump(blank_config, f)
+            else:
+                raise Exception('Could not load config file: File does not exit.')
+
+        with open(self.config_path, 'r') as config_file:
+            self.config = json.load(config_file)
+
+    def addWatchFile(self, filename, destination):
+        if destination in self.config['watch']:
+            if not filename in self.config['watch'][destination]:
+                self.config['watch'][destination].append(filename)
+        else:
+            self.config['watch'][destination] = [filename]
+
+    def removeWatchFile(self, filename):
+        for dest in self.config['watch']:
+            if filename in self.config['watch'][dest]:
+                self.config['watch'][dest].remove(filename)
+
+    def addIgnoreFile(self, filename):
+        if not filename in self.config['ignore']:
+            self.config['ignore'].append(filename)
+
+    def removeIgnoreFile(self, filename):
+        if filename in self.config['ignore']:
+            self.config['ignore'].remove(filename)
+
+    def writeConfigFile(self):
+        with open(self.config_path, 'w') as config_file:
+            json.dump(self.config, config_file)
+
+
 def getFilePaths(source_dir, dest_dir, filenames, recursive=False):
     all_files = []
     for name in filenames:
@@ -60,8 +131,8 @@ def main():
         files to watch and where to copy them to.
     """)
 
-    parser.add_argument('find', help="The source directory to look for watch files in.")
-    parser.add_argument('dest', help='The target directory to copy the watched files to when changes are detected.')
+    parser.add_argument('find', help="The source directory to look for watch files in.", nargs='?', default=0)
+    parser.add_argument('dest', help='The target directory to copy the watched files to when changes are detected.', nargs='?', default=0)
     parser.add_argument('-n', '--name', help='The name(s) of the files or pattern to use for finding files to watch.', nargs='+')
     parser.add_argument('-r', '--recursive', 
         help="""
@@ -74,17 +145,31 @@ def main():
     )
 
     # Config file arguments
-    parser.add_argument('-a', '--add', help="Add file(s) and destinations to config file watch-list/ignore-list")
-    parser.add_argument('-d', '--delete', help="Remove file(s) and destinations from config file watch-list/ignore-list")
-    parser.add_argument('-i', '--ignore', help="This flag will add/remove files to/from the ignore list. Files in the ignore list will never be watched for changes.")
-    parser.add_argument('-c', '--conf', help="Start file watcher using settings in watch.config.json file to determine source files to watch and destinations to copy to.")
+    parser.add_argument('-c', '--conf', help="Start file watcher using settings in wac.config.json file to determine source files to watch and destinations to copy to.", action='store_true')
+
+    config_group = parser.add_mutually_exclusive_group()
+    config_group.add_argument('-a', '--add', help="Add file(s) and destinations to config file watch-list/ignore-list", action='store_true')
+    config_group.add_argument('-d', '--delete', help="Remove file(s) and destinations from config file watch-list/ignore-list", action='store_true')
+    config_group.add_argument('-i', '--ignore', help="This flag will add/remove files to/from the ignore list. Files in the ignore list will never be watched for changes.", action='store_true')
 
     args = parser.parse_args()
+
+    if args.find and not args.dest:
+        raise Exception('A destination must be provided for watch files.')
+    elif args.conf or (not args.find and not args.dest):
+        pass
 
     source_dir = Path(args.find)
     dest_dir = Path(args.dest)
 
     files = getFilePaths(source_dir, dest_dir, args.name, args.recursive)
+
+    if args.add:
+        pass
+    elif args.delete:
+        pass
+    elif args.ignore:
+        pass
 
     watcher = FileChangeWatcher(files, dest_dir)
     watcher.startWatch()
